49a56c4ebeefdcbfa676f18d909fb085156df353
AaqilBaig <135568009+AaqilBaig@users.noreply.github.com>
2024-12-24 19:23:48 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173','https://cms-wine-delta.vercel.app/'],
+  origin: ['http://localhost:5173','https://tsig.onrender.com'],
   credentials: true,
 }));
 app.use(express.json());
