59d472f8a8f8a186e3cce218b4a1a0f788837c9f
AaqilBaig <135568009+AaqilBaig@users.noreply.github.com>
2024-12-24 18:52:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import routes from '../routes/index.js';
+import routes from './routes/index.js';
 
 // Load environment variables first
 dotenv.config();
