ec2f08ae06c8dba2e2be83636f6038c07777d45d
Shaik Abrar <78590172+devShaik010@users.noreply.github.com>
2025-01-07 18:23:41 +0530

    end point changed by railway...

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setLoading(true);
     
     try {
-      const response = await fetch('https://cms-38xq.onrender.com/api/users/signin', {
+      const response = await fetch('https://railway-production-dc61.up.railway.app/api/users/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
