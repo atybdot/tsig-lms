c993d039367d6b26a697268743c3f3cf6c2ebd4e
AaqilBaig <aaqiladilbaig@gmail.com>
2025-06-26 16:47:18 +0530

    prod fix

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,17 @@
-import { Router } from 'express';
+import express from 'express';
+import adminRoutes from './adminRoutes.js';
 import userRoutes from './userRoutes.js';
+import taskRoutes from './taskRoutes.js';
+import completedTaskRoutes from './completedTaskRoutes.js';
 import cronRoutes from './cronRoutes.js';
 
-const router = Router();
+const router = express.Router();
 
+// Mount routes
+router.use('/admin', adminRoutes);
 router.use('/users', userRoutes);
+router.use('/tasks', taskRoutes);
+router.use('/completed-tasks', completedTaskRoutes);
 router.use('/cron', cronRoutes);
 
 export default router;
